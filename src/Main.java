2e21ec029d5ee4ecb2be54c3ef09c9de88d4ba42
dmsthf2000 <dsmthf200099@gmail.com>
2022-11-02 16:27:31 +0900

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import m2.view.MainView;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MainView mv = new MainView();
     }
 }
