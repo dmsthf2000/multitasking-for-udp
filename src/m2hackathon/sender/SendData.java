3777027e14a1fcfc08d354ad1d97ab289d894ff9
[sooah1202] <[001202s@gmail.com]>
2022-11-03 15:33:39 +0900

    coding the send DatagramPacket

--- src/m2hackathon/sender/SendData.java
+++ src/m2hackathon/sender/SendData.java
@@ -4,20 +4,42 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 
 public class SendData
 {
-    private static void sendDataToRecevier(MulticastSocket mulSocket, String data, InetAddress multicastAddress, int multicastPort)
+    private void sendDataToRecevier(MulticastSocket mulSocket, InetAddress multicastAddress, int multicastPort, String dataMsg)
     {
-        DatagramPacket sendPacket = new DatagramPacket(data.getBytes(), data.length(), multicastAddress, multicastPort);
+        String localAddress = "";
+        try
+        {
+            localAddress = InetAddress.getLocalHost().getHostAddress();
+        }
+        catch (UnknownHostException e) {throw new RuntimeException(e);}
+
+        //startMsg
+        String startMsg = "start / " + localAddress;
+        DatagramPacket startPacket = makePacket(startMsg, multicastAddress, multicastPort);
+
+        //dataMsg
+        DatagramPacket dataPacket = makePacket(dataMsg, multicastAddress, multicastPort);
+
+        //endMsg
+        String endMsg = "end / " + localAddress;
+        DatagramPacket endPacket = makePacket(endMsg, multicastAddress, multicastPort);
 
         try
         {
-            mulSocket.send(sendPacket);
-        }
-        catch (IOException e)
-        {
+            mulSocket.send(startPacket);
+            mulSocket.send(dataPacket);
+            mulSocket.send(endPacket);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    private DatagramPacket makePacket(String sendMsg, InetAddress multicastAddress, int multicastPort)
+    {
+        DatagramPacket sendPacket = new DatagramPacket(sendMsg.getBytes(), sendMsg.length(), multicastAddress, multicastPort);
+        return sendPacket;
     }
 }
