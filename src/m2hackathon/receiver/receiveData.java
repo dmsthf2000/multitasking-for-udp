5f3dca236cb622cf2a6d347a409f227c2c2236db
[sooah1202] <[001202s@gmail.com]>
2022-11-03 16:38:36 +0900

    create receive data method

--- src/m2hackathon/receiver/receiveData.java
+++ src/m2hackathon/receiver/receiveData.java
@@ -1,6 +1,38 @@
 package m2hackathon.receiver;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+import java.net.SocketAddress;
+
 public class receiveData
 {
+    DatagramPacket receivedPacket;
 
+    public void receiveDataToSender(MulticastSocket mulSocket)
+    {
+        byte[] receivedData = new byte[65508];
+        receivedPacket = new DatagramPacket(receivedData, receivedData.length);
+        try {
+            mulSocket.receive(receivedPacket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SocketAddress getSender()
+    {
+        if(receivedPacket == null)
+            return null;
+
+        return receivedPacket.getSocketAddress();
+    }
+
+    public String getData()
+    {
+        if(receivedPacket == null || receivedPacket.getData().length < 1)
+            return null;
+
+        return new String(receivedPacket.getData()).trim();
+    }
 }
