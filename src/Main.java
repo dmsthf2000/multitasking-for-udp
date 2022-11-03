50c647d82a0425d634886c29b881ed1cfc36b5c5
byeoli <98lim511@naver.com>
2022-11-03 16:07:37 +0900

    make the multicastsocket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import m2hackathon.accessMulticast.SetMultiSocket;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        MulticastSocket teamSocket = null;
+
+
+        //gui
+        String team = "";
+
+        //멀티캐스트 팀소켓 생성
+        SetMultiSocket sm = new SetMultiSocket();
+        teamSocket = sm.getMulticastSocket(team);
+
+        //임의의 멀티캐스트 ip 지정
+        try {
+            InetAddress address = InetAddress.getByName("224.128.1.5");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/m2hackathon/accessMulticast/SetMultiSocket.java
@@ -0,0 +1,66 @@
+package m2hackathon.accessMulticast;
+
+import java.io.IOException;
+import java.net.MulticastSocket;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetMultiSocket
+{
+    public MulticastSocket getMulticastSocket(String team)
+    {
+        MulticastSocket socket = null;
+
+        try
+        {
+            //port 가져와서 멀티캐스트 생성
+            socket = new MulticastSocket(setPort(team));
+        }
+        catch (UnknownHostException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return socket;
+    }
+
+    //GUI에서 받은 값으로 임의의 포트를 지정
+    private int setPort(String team) {
+        switch (team){
+            case "A":
+                return 3000;
+            case "B":
+                return 3001;
+            case "C":
+                return 3002;
+            default:
+                return -1;
+        }
+    }
+
+    //사용 가능한 포트번호를 리스트로 출력(추후에 사용 예정)
+//    public List<Integer> AvailablePort() {
+//        List<Integer> portArray = new ArrayList<>();
+//        for (int i = 1024; i < 65536; i++) {
+//            try {
+//                ServerSocket ss = new ServerSocket(i);
+//                ss.close();
+//            } catch (IOException ee) {
+//                portArray.add(i);
+//                break;
+//            }
+//        }
+//        return portArray;
+//    }
+}
+
+
+
+
+
