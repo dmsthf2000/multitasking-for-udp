c0e1d3ab6d5dbd86067a4b3eaab1b3d0c9b8be7c
byeoli <98lim511@naver.com>
2022-11-03 16:34:11 +0900

    test2

--- src/m2hackathon/accessMulticast/AccessMulticastMethod.java
+++ src/m2hackathon/accessMulticast/AccessMulticastMethod.java
@@ -1,10 +1,17 @@
 package m2hackathon.accessMulticast;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccessMulticastMethod {
-    public String getServerIp() {
+//    private int port;
+
+    //내 컴퓨터 ip주소
+    public String getLocalIp() {
 
         InetAddress local = null;
         try {
@@ -19,14 +26,32 @@ public class AccessMulticastMethod {
         }
         else {
             String ip = local.getHostAddress();
-            String result = ip.substring(ip.length()-3, ip.length());
-            return result; //내 ip주소에서 마지막 3글자 출력
+            return ip; //내 ip주소에서 마지막 3글자 출력
         }
     }
+
+
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        //내 ip주소 확인
         AccessMulticastMethod a = new AccessMulticastMethod();
-        a.getServerIp();
-        System.out.println(a.getServerIp());
+        a.getLocalIp();
+        System.out.println(a.getLocalIp());
+
+
+//        //현재 열려있는 포트 확인
+//        for(int i = 1024; i < 65536; i++){
+//            try{
+//                ServerSocket ss = new ServerSocket(i);
+//                    ss.close();
+//            }catch (IOException ee){
+//                System.out.println(i);
+//            }
+//        }
+//        List<Integer> intar = a.AvailablePort();
+//        System.out.println(intar.toString());
+
+//        System.out.println(a.AvailablePort());
     }
 }
